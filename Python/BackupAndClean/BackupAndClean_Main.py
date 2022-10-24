c91fe225a4e017dd0398abece306510d2f3a2b22
Alex Billingham <alexbillingham@dsgroup.uk.com>
2022-10-24 11:53:34 +0100

    BackupAndClean code changes

--- Python/BackupAndClean/BackupAndClean_Main.py
+++ Python/BackupAndClean/BackupAndClean_Main.py
@@ -15,11 +15,13 @@ def load_config():
         print("Loading config...")
         cp = ConfigParserXML()
         if(cp.is_backup_enabled()):
+            print("Running backup...")
             backup_to, backup_paths = cp.get_backup_config()
             print(backup_to)
             print(backup_paths)
         
         if(cp.is_spring_clean_enabled()):
+            print("Cleaning...")
             delete_older_than, filter, clean_paths = cp.get_spring_clean_config()
             print(delete_older_than)
             print(filter)
